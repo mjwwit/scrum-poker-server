f16ef168525e9875bf5c695d5432f1254e6fa858
hkiichi <iris.kikuchi@gmail.com>
2019-07-31 10:48:29 +0200

    implemented comments

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require('express')
-var app = express()
-var port = 5000
+const express = require('express')
+const app = express()
+const port = 5000
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+app.get('/', (req, res) =>
+  res.send('hello world'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
